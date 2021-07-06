874a82413b5d70bcc2f26d46714fbd4158d6351c
stutisaurya <stuti.saurya@gmail.com>
2021-07-06 23:03:46 +0530

    Added:SpaceBetweenThreeNumber

--- src/com/PinCode.java
+++ src/com/PinCode.java
@@ -6,8 +6,8 @@ import java.util.regex.Pattern;
 public class PinCode {
 	public static void main(String[] args) {
 
-		String pinCode = "400088B";
-		String regex = "^[1-9]{1}[0-9]{5}$";
+		String pinCode = "400 088";
+		String regex = "^[1-9]{1}[0-9]{2}\\s{0,1}[0-9]{3}$";
 		Pattern pattern = Pattern.compile(regex);
 		Matcher matcher = pattern.matcher(pinCode);
 		boolean result = matcher.matches();
