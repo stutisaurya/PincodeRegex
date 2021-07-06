d3c2392701434f94a1c44a89b4d99fc1b3c9b188
stutisaurya <stuti.saurya@gmail.com>
2021-07-06 22:49:17 +0530

    Added:ValidatePincode

---
+++ src/com/PinCode.java
@@ -0,0 +1,19 @@
+package com;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PinCode {
+	public static void main(String[] args) {
+
+		String pinCode = "400088";
+		String regex = "^[1-9]{1}[0-9]{5}$";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(pinCode);
+		boolean result = matcher.matches();
+		if (result)
+			System.out.println("Valid PinCode");
+		else
+			System.out.println("InValid PinCode");
+	}
+}
